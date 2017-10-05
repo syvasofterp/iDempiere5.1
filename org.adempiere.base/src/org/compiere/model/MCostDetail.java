8fb0652cf4cc9f667a8c3f6a2dad750fd46192d1
Syed <Syed@localhost>
2017-10-05 23:07:29 +0530

    Added Cost Generation for Boulder Receipt Document.

--- org.adempiere.base/src/org/compiere/model/MCostDetail.java
+++ org.adempiere.base/src/org/compiere/model/MCostDetail.java
@@ -57,7 +57,90 @@ public class MCostDetail extends X_M_CostDetail
 			"INNER JOIN M_InOutLine iol ON io.M_InOut_ID=iol.M_InOut_ID " +
 			"INNER JOIN C_DocType c ON io.C_DocType_ID=c.C_DocType_ID " +
 			"WHERE iol.M_InOutLine_ID=?";
-			
+	
+	// SyvaSoft
+	/**
+	 * 	Is BoulderReceipt
+	 *	@return true if invoice line
+	 */
+	public boolean isBoulderReceipt()
+	{
+		return getTF_Boulder_Receipt_ID() != 0;
+	}	//	isInvoice
+
+	// SyvaSoft
+	public int getTF_Boulder_Receipt_ID() {
+		return get_ValueAsInt("TF_Boulder_Receipt_ID");
+	}
+	
+	/** By SyvaSoft..
+	 * 	Create New Boulder Receipt Cost Detail.
+	 * 	Called from Doc_Invoice - for Invoice Adjustments
+	 *	@param as accounting schema
+	 *	@param AD_Org_ID org
+	 *	@param M_Product_ID product
+	 *	@param M_AttributeSetInstance_ID asi
+	 *	@param C_InvoiceLine_ID invoice
+	 *	@param M_CostElement_ID optional cost element for Freight
+	 *	@param Amt amt
+	 *	@param Qty qty
+	 *	@param Description optional description
+	 *	@param trxName transaction
+	 *	@return true if created
+	 */
+	public static boolean createBoulderReceipt (MAcctSchema as, int AD_Org_ID, 
+		int M_Product_ID, int M_AttributeSetInstance_ID,
+		int TF_Boulder_Receipt_ID, int M_CostElement_ID, 
+		BigDecimal Amt, BigDecimal Qty,
+		String Description, String trxName)
+	{
+		MCostDetail cd = get (as.getCtx(), "TF_Boulder_Receipt_ID=? AND Coalesce(M_CostElement_ID,0)="+M_CostElement_ID+" AND M_Product_ID="+M_Product_ID, 
+			TF_Boulder_Receipt_ID, M_AttributeSetInstance_ID, as.getC_AcctSchema_ID(), trxName);
+		//
+		if (cd == null)		//	createNew
+		{
+			cd = new MCostDetail (as, AD_Org_ID, 
+				M_Product_ID, M_AttributeSetInstance_ID, 
+				M_CostElement_ID, 
+				Amt, Qty, Description, trxName);
+			cd.set_ValueOfColumn("TF_Boulder_Receipt_ID", TF_Boulder_Receipt_ID);
+		}
+		else
+		{
+			if (cd.isProcessed())
+			{
+				// MZ Goodwill
+				// set deltaAmt=Amt, deltaQty=qty, and set Cost Detail for Amt and Qty	 
+				cd.setDeltaAmt(Amt.subtract(cd.getAmt()));
+				cd.setDeltaQty(Qty.subtract(cd.getQty()));
+			}
+			else
+			{
+				cd.setDeltaAmt(BigDecimal.ZERO);
+				cd.setDeltaQty(BigDecimal.ZERO);
+				cd.setAmt(Amt);
+				cd.setQty(Qty);
+			}
+			if (cd.isDelta())
+			{
+				cd.setProcessed(false);
+				cd.setAmt(Amt);
+				cd.setQty(Qty);
+			}
+			// end MZ
+			else if (cd.isProcessed())
+				return true;	//	nothing to do
+		}
+		boolean ok = cd.save();
+		if (ok && !cd.isProcessed())
+		{
+			ok = cd.process();
+		}
+		if (s_log.isLoggable(Level.CONFIG)) s_log.config("(" + ok + ") " + cd);
+		return ok;
+	}	//	createBoulder Receipt
+	
+	
 	/**
 	 * 	Create New Order Cost Detail for Purchase Orders.
 	 * 	Called from Doc_MatchPO
