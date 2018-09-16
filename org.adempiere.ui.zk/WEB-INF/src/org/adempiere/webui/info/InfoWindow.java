44e8026273c009268316813296b5c9b9d1bc64e5
adempieresyed <adempieresyed@localhost>
2018-09-16 14:33:26 +0530

    Query Data On Load for InfoWindow is added.

--- org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/info/InfoWindow.java
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/info/InfoWindow.java
@@ -136,6 +136,8 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 	private List<GridField> gridFields;
 	private Checkbox checkAND;
 	
+	private boolean m_QueryDataOnLoad = false;
+	
 	/**
 	 * Menu contail process menu item
 	 */
@@ -182,6 +184,10 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 		this.m_gridfield = field;
 		this.queryValue = queryValue;
 
+		//Syed on 1/3/2018
+		if(infoWindow != null)
+			this.m_QueryDataOnLoad = infoWindow.get_ValueAsBoolean("QueryDataOnLoad");
+				
    		//Xolali IDEMPIERE-1045
    		contentPanel.addActionListener(new EventListener<Event>() {
    			public void onEvent(Event event) throws Exception {
@@ -213,7 +219,12 @@ public class InfoWindow extends InfoPanel implements ValueChangeListener, EventL
 			{
 				prepareTable();
 				processQueryValue();
-			}			
+			}		
+			
+			//Syed on 1/3/2018
+			if(this.m_QueryDataOnLoad) {
+				onUserQuery();
+			}
 		}
 		
 		if (ClientInfo.isMobile()) {
